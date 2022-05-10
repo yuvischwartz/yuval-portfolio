4208b6458ba9ebfe6422512500a46ef10eceeb40
yuvischwartz <yuval.orderdesk@gmail.com>
2022-05-10 00:42:54 -0200

    changes

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="h-screen w-screen">
       <Navbar />
-      <div className={'flex sm:flex-row flex-col'}>
-        <div className={'sm:hidden inline-block justify-center items-center flex'}>
+      <div className={'flex sm:flex-row flex-col bg-gray-100'}>
+        <div className={'sm:hidden inline-block justify-center items-center flex m-10'}>
         <img className={'h-48 w-48 rounded-full border-4 border-black'} alt='img' src={`images/img.png`} />
         </div>
         <div className={'h-screen w-screen sm:w-1/2'}>
@@ -16,19 +16,21 @@ function App() {
           <p className={'body-lite text-xl pt-2 text-left sm:ml-36 ml-10'}>Familiar with a wide range of programming utilities and languages.</p>
           <p className={'body-lite text-xl pt-2 text-left sm:ml-36 ml-10'}>Knowledgeable of backend and frontend development requirements.</p>
           <p className={'body-lite text-xl pt-2 text-left sm:ml-36 ml-10'}>Complex problem-solver with an analytical and driven mindset.</p>
-          <div className={'flex flex-row mt-5 sm:ml-36 ml-10'}>
-            <button className={'bg_blue py-1 px-6 rounded-full text-white body-lite m-1 w-32 hover:bg-black'}>#HTML</button>
-            <button className={'bg_green py-1 px-6 rounded-full text-white body-lite m-1 w-32 hover:bg-black'}>#CSS</button>
-            <button className={'bg_pink py-1 px-6 rounded-full text-white body-lite m-1 w-32 hover:bg-black'}>#C</button>
+          <div className={'flex flex-row mt-5 sm:ml-36 ml-8'}>
+            <button className={'bg_blue py-1 px-6 rounded-full text-white body-lite m-1 w-28 sm:w-32 hover:bg-black'}>#HTML</button>
+            <button className={'bg_green py-1 px-6 rounded-full text-white body-lite m-1 w-28 sm:w-32 hover:bg-black'}>#CSS</button>
+            <button className={'bg_pink py-1 px-6 rounded-full text-white body-lite m-1 w-28 sm:w-32 hover:bg-black'}>#C/C++</button>
+            <button className={'bg_purple py-1 px-6 rounded-full text-white body-lite m-1 w-28 sm:w-32 hover:bg-black'}>#Git</button>
           </div>
-          <div className={'flex flex-row mt-1 sm:ml-36 ml-10'}>
-            <button className={'bg_purple py-1 px-6 rounded-full text-white body-lite m-1 w-32 hover:bg-black'}>#React</button>
-            <button className={'bg_blue2 py-1 px-6 rounded-full text-white body-lite m-1 w-32 hover:bg-black'}>#JavaScript</button>
-            <button className={'bg_blue py-1 px-6 rounded-full text-white body-lite m-1 w-32 hover:bg-black'}>#Node.js</button>
+          <div className={'flex flex-row mt-1 sm:ml-36 ml-8'}>
+            <button className={'bg_purple py-1 px-6 rounded-full text-white body-lite m-1  w-28 sm:w-32 hover:bg-black'}>#React</button>
+            <button className={'bg_blue2 py-1 px-6 rounded-full text-white body-lite m-1 w-28 sm:w-32 hover:bg-black'}>#JavaScript</button>
+            <button className={'bg_blue py-1 px-6 rounded-full text-white body-lite m-1 w-28 sm:w-32 hover:bg-black'}>#Node.js</button>
+            <button className={'bg_green py-1 px-6 rounded-full text-white body-lite m-1 w-28 sm:w-32 hover:bg-black'}>#SQL</button>
           </div>
           <p className={'mt-5 sm:ml-36 ml-10 body-lite text-xl border-b border-black hover:border-b-4 cursor-pointer hover:font-medium'}>Check out my work</p>
         </div>
-        <div className={'hidden sm:inline-block h-screen w-1/2 border border-black'}>
+        <div className={'hidden sm:inline-block h-screen w-1/2'}>
           <img className={'w-full'} alt='img' src={`images/img.png`} />
         </div>
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className={'h-20 border-black border'}>
-          NAVBAR
-               
+    <div className={'h-18 border-black'}>
+          {/* <h1 className={'title2 text-3xl text-left ml-8 sm:ml-36 m-3'}>YS.</h1>  */}
+          <ul className={'flex justify-end'}>
+          <h1 className={'flex-auto title2 text-3xl text-left ml-8 sm:ml-36 m-4 cursor-pointer'}>YS.</h1>
+            <li className={'lite2 my-4 text-lg mr-5 sm:mr-24 hover:border-b border-black cursor-pointer'}>Portfolio</li>
+            <li className={'lite2 my-4 text-lg  mr-5 sm:mr-24 hover:border-b border-black cursor-pointer'}>Resume</li>
+            <li className={'lite2  text-lg my-4 mr-8 sm:mr-36 hover:border-b border-black cursor-pointer'}>Contact</li>
+          </ul>      
     </div>
   )
 }
